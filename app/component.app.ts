a318e75a3539a1e253844e709780f4434c1fccd6
Istvan Marki <istv0050@edu.easj.dk>
2017-04-12 10:33:03 +0200

    inital upload commit

---
+++ app/component.app.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: './partials/app.html'
+})
+
+export class AppComponent {
+    name: string;
+    artists: any;
+
+    constructor() {
+        this.name = 'Xhou Ta';
+        this.artists = [
+            {
+                name: 'Barot Bellingham',
+                school: 'Penn State'
+            }, {
+                name: 'Jonathan Ferra',
+                school: 'University of Illinois'
+            }, {
+                name: 'Hillary Post',
+                school: 'University of Florida'
+            }
+
+        ]
+    }
+
+}
