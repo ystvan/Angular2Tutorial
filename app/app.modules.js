1d9bd7b6c3153e737d7e55d8a7c5782745868fb3
Istvan Marki <istv0050@edu.easj.dk>
2017-04-12 11:17:02 +0200

    two-way bindings

--- app/app.modules.js
+++ app/app.modules.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '@angular/platform-browser', './component.app'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/platform-browser', '@angular/forms', './component.app'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['@angular/core', '@angular/platform-browser', './component.app'
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, platform_browser_1, component_app_1;
+    var core_1, platform_browser_1, forms_1, component_app_1;
     var AppModule;
     return {
         setters:[
@@ -19,6 +19,9 @@ System.register(['@angular/core', '@angular/platform-browser', './component.app'
             },
             function (platform_browser_1_1) {
                 platform_browser_1 = platform_browser_1_1;
+            },
+            function (forms_1_1) {
+                forms_1 = forms_1_1;
             },
             function (component_app_1_1) {
                 component_app_1 = component_app_1_1;
@@ -29,7 +32,8 @@ System.register(['@angular/core', '@angular/platform-browser', './component.app'
             AppModule = __decorate([
                 core_1.NgModule({
                     imports: [
-                        platform_browser_1.BrowserModule
+                        platform_browser_1.BrowserModule,
+                        forms_1.FormsModule
                     ],
                     declarations: [
                         component_app_1.AppComponent
